ae29543287006a475b162a23b4cb40e8bf1340d8
Flarome <developer@flarome.com>
2024-12-16 14:55:56 +0100

    jkj

--- start-with-docker.sh
+++ start-with-docker.sh
@@ -2,25 +2,27 @@
 
 # Vérification de Docker
 echo "Mise à jour des paquets et installation de Docker..."
+
+# Mettre à jour les paquets en tant que root
 apt-get update && apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
-sudo apt-get update
-sudo apt-get install -y docker-ce
+apt-get update
+apt-get install -y docker-ce
 docker --version
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo usermod -aG docker $USER
+systemctl start docker
+systemctl enable docker
+usermod -aG docker $USER
 
 # Construire l'image Docker à partir du Dockerfile
 echo "Construction de l'image Docker à partir du Dockerfile..."
-sudo docker build -t shopify-app .
+docker build -t shopify-app .
 
 # Démarrer le conteneur en respectant le Dockerfile
 echo "Démarrage du conteneur Docker..."
-sudo docker run shopify-app
+docker run shopify-app
 
 # Garder le processus actif
 tail -f /dev/null
