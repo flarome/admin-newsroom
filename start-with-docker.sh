7ffd3c016b8fe2db8915599e145cdeed741af3b3
Flarome <developer@flarome.com>
2024-12-16 14:35:48 +0100

    k

--- start-with-docker.sh
+++ start-with-docker.sh
@@ -4,16 +4,16 @@
 echo "Vérification de Docker..."
 if ! [ -x "$(command -v docker)" ]; then
   echo "Docker n'est pas installé. Installation..."
-  curl -fsSL https://get.docker.com | sh
+  sudo curl -fsSL https://get.docker.com | sudo sh
 fi
 
 # Construire l'image Docker à partir du Dockerfile
 echo "Construction de l'image Docker à partir du Dockerfile..."
-docker build -t shopify-app .
+sudo docker build -t shopify-app .
 
 # Démarrer le conteneur en respectant le Dockerfile
 echo "Démarrage du conteneur Docker..."
-docker run shopify-app
+sudo docker run shopify-app
 
 # Garder le processus actif
 tail -f /dev/null
