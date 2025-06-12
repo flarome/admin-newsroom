9f1bad59a4e2d76c16b6c33324aa3c3663853a98
flaromedeveloper <developer@flarome.com>
2025-06-13 01:08:29 +0200

    re

--- app/VPE/src/app/editor/page.tsx
+++ app/VPE/src/app/editor/page.tsx
@@ -2,6 +2,7 @@ import { Toaster } from 'sonner';
 
 import { PlateEditor } from '@/components/editor/plate-editor';
 
+import "katex/dist/katex.min.css";
 
 export default function Page() {
   return (
