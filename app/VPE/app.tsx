fcd70e38da35084d7817693fec54a9741a2c2bb9
flaromedeveloper <developer@flarome.com>
2025-06-13 02:06:22 +0200

    re

--- app/VPE/app.tsx
+++ app/VPE/app.tsx
@@ -3,13 +3,28 @@ import './styles/globals.css'
 import App from './src/app/editor/page' 
 import {ClientOnly} from '../components/utils/client-only';
 
+import { useEffect, useState } from "react";
+
+const APP1 = (props) => {
+
+  const [mounted, setMounted] = useState(false);
+  useEffect(() => setMounted(true), []);
+   if (!mounted) return <div>Chargement en cours...</div>;
+     
+    return (<App {...props} />)
+
+}
+
+
 const Main = (props) => {
 
+     
     return (<div data-cms="vpe">
         
          <ClientOnly fallback={<div>Chargement en cours...</div>}>
-    {() => <App {...props} />}
+    {() => <APP1 {...props} />}
   </ClientOnly>
+
 
         </div>)
 
